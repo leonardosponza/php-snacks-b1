e74d3e2944a96656532ca7f8d3d4162a28d5aa93
leonardo <sponzal4@gmail.com>
2021-07-26 17:53:27 +0200

    snack1

--- my_script.php
+++ my_script.php
@@ -5,10 +5,13 @@ casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite co
 <!-- Olimpia Milano - Cantù | 55-60 -->
 
 <?php
-
+$array = [
+   'squad1' => 'Olimpia Milano - Cantù | 55-60',
+   'squad2' => 'macerata - livorno | 48-39',
+   'squad3' => 'napoli - firenze | 34-55'
+]
 
 ?>
-
 
 
 <!DOCTYPE html>
@@ -20,6 +23,14 @@ casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite co
     <title>Document</title>
 </head>
 <body>
-    
+    <p>partita:<?php echo $array['squad1']?></p>
+    <p>partita:<?php echo $array['squad2']?></p>
+    <p>partita:<?php echo $array['squad3']?></p>
 </body>
 </html>
+
+<!-- Snack 2 -->
+<!-- Passare come parametri GET name, mail e age
+ e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
+ che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, 
+ altrimenti “Accesso negato” -->
