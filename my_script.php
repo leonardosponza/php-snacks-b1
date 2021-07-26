f398f0567bb0d4928cecb73e0ce5b3a16cfc627e
leonardo <sponzal4@gmail.com>
2021-07-26 18:01:26 +0200

    snack1 changes

--- my_script.php
+++ my_script.php
@@ -4,11 +4,12 @@ casa e una squadra ospite, punti fatti dalla squadra di
 casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema. -->
 <!-- Olimpia Milano - Cantù | 55-60 -->
 
-<?php
+ <!-- <?php
 $array = [
-   'squad1' => 'Olimpia Milano - Cantù | 55-60',
-   'squad2' => 'macerata - livorno | 48-39',
-   'squad3' => 'napoli - firenze | 34-55'
+   'team_casa'=> 'Milano'
+   'team_visitatore'=>'Cantù'
+   'punti_casa'=>'55'
+   'punti_visitatore'=>'60'
 ]
 
 ?>
@@ -23,14 +24,33 @@ $array = [
     <title>Document</title>
 </head>
 <body>
-    <p>partita:<?php echo $array['squad1']?></p>
-    <p>partita:<?php echo $array['squad2']?></p>
-    <p>partita:<?php echo $array['squad3']?></p>
+    <span>partita:<?php echo $array['team_casa']?> - </span>
+    <span>partita:<?php echo $array['team_visitatore']?></span>
+    <span>partita:<?php echo $array['punti_casa']?> - </span>
+    <span>partita:<?php echo $array['punti_visitatore']?></span>
 </body>
-</html>
+</html> --> 
 
 <!-- Snack 2 -->
 <!-- Passare come parametri GET name, mail e age
  e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
  che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, 
  altrimenti “Accesso negato” -->
+
+ <?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
